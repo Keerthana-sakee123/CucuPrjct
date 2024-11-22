b4173bf7cf12e52773f92ee55d26865c70664465
sakthimani@evolute-fintech.in <sakthimani@evolute-fintech.in>
2024-11-22 16:53:55 +0530

    2nd commit

--- src/test/java/org/sample/TestRunner.java
+++ src/test/java/org/sample/TestRunner.java
@@ -1,5 +1,13 @@
 package org.sample;
 
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "D:\\Keerthi_Java\\CucuPrjt\\src\\test\\resources\\FbLogin.feature",
+				glue = "org.script")
 public class TestRunner {
 
 }
